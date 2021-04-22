1ff4e2a74385b18987cb34cceeceea5995dd7e07
Valikhan Akhmedov <valikhan.akhmedov@aitupay.kz>
2021-04-22 16:46:23 +0600

    chore: eject sample app

--- samples/sample-app/src/App.tsx
+++ samples/sample-app/src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {
   Routing,
@@ -8,6 +7,7 @@ import {
   PanelHeader,
   useRouter,
 } from 'react-wv-navigation';
+import { Button } from '@material-ui/core';
 
 const ROUTERS: IRouterOptions[] = [
   {},
@@ -18,26 +18,41 @@ const ROUTERS: IRouterOptions[] = [
 ];
 
 function App() {
+  return (
+    <Routing routers={ROUTERS}>
+      <Main />
+    </Routing>
+  );
+}
+
+const Main: React.FC = () => {
   const { pushScreen } = useRouter();
 
   useEffect(() => {
     pushScreen({ key: '1', screen: <LongTextScreen /> });
   }, [pushScreen]);
 
-  return <Routing routers={ROUTERS}></Routing>;
-}
+  return null;
+};
 
 export default App;
 
 const LongTextScreen: React.FC = () => {
+  const { pushScreen, popScreen } = useRouter();
+
   return (
     <>
-      <PanelHeader></PanelHeader>
+      <PanelHeader leftAction='back' onLeftAction={popScreen}></PanelHeader>
       <PanelBody>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita vitae,
         voluptatibus beatae fuga dolorum sapiente, nam quae amet aliquam
         necessitatibus ipsa consequatur illo animi ratione, suscipit impedit
         exercitationem quos dolore.
+        <button
+          onClick={() => pushScreen({ key: '2', screen: <LongTextScreen /> })}
+        >
+          Next
+        </button>
       </PanelBody>
     </>
   );
